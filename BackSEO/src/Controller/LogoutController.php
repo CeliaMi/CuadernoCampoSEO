b491300f6b2ca92c6c3c8d8e2ddb9f43ff8edb18
Celia <celiamillanalemany@gmail.com>
2022-11-09 14:10:14 +0100

    paginaInicioRutasDesvinculadasUsuarioGeneral+UsuarioAdmin

--- BackSEO/src/Controller/LogoutController.php
+++ BackSEO/src/Controller/LogoutController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SecurityController extends AbstractController
+class LogoutController extends AbstractController
 {
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout()
---
+++ BackSEO/src/Controller/NuevaAlertaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Alerta;
+use App\Form\AlertaType;
+use App\Repository\AlertaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NuevaAlertaController extends AbstractController
+{
+    #[Route('/new', name: 'app_alerta_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, AlertaRepository $alertaRepository): Response
+    {
+        $alertum = new Alerta();
+        $form = $this->createForm(AlertaType::class, $alertum);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $alertaRepository->save($alertum, true);
+
+            return $this->redirectToRoute('app_inicio', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('/new.html.twig', [
+            'alertum' => $alertum,
+            'form' => $form,
+        ]);
+    }
+}
