0b747a67da714852c92b38046320ef632667b662
Oli <oliris.of@gmail.com>
2022-11-16 12:24:52 +0100

    mensajeBorradoDeForm

--- BackSEO/migrations/Version20221116112015.php
+++ BackSEO/migrations/Version20221116112015.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221116110654 extends AbstractMigration
+final class Version20221116112015 extends AbstractMigration
 {
     public function getDescription(): string
     {
