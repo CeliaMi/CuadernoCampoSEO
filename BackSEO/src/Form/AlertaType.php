5268f854f9746dea1954aaeb4761da02e9499e30
Celia <celiamillanalemany@gmail.com>
2022-11-18 13:02:51 +0100

    mapaRellenaInputRealizado

--- BackSEO/src/Form/AlertaType.php
+++ BackSEO/src/Form/AlertaType.php
@@ -9,6 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Validator\Constraints\File;
@@ -21,7 +22,7 @@ class AlertaType extends AbstractType
             ->add('tipoAmenaza', ChoiceType::class, [
                 'choices'=>Alerta::TIPOAMENAZA  
                 ])
-            ->add('ubicacion') 
+            ->add('ubicacion')
             ->add('foto', FileType::class, [
                 'label' => 'Añadir foto',
                 'mapped' => false,
