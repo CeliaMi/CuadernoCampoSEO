a1bc75a83b606abd74d208b1a7d67f7d2db583fd
Celia <celiamillanalemany@gmail.com>
2022-11-19 18:03:48 +0100

    ResponseJsonController+ResponseXMLController

--- BackSEO/src/Controller/ResponseJsonController.php
+++ BackSEO/src/Controller/ResponseJsonController.php
@@ -30,27 +30,4 @@ class ResponseJsonController extends AbstractController
         return $this->json($data);
         
     }
-
-    
-    #[Route('/XML', name: 'response_XML')]
-    public function ResponseXML(Request $request, AlertaRepository $alertaRepository): Response
-    {   
-        $amenaza = $this->getDoctrine()
-        ->getRepository(Alerta::class)
-        ->findAll();
-        $data = [];
-        foreach ($amenaza as $p) {
-            $data[] = [
-                'id' => $p->getId(),
-                'TipoAlerta' => $p->getTipoAmenaza(),
-                'Descripcion'=> $p ->getDescripcion(),
-            ];
-            // return $this->json($data);
-            
-        $xml = new XmlEncoder(json($data));
-        array_walk_recursive($test_array, array ($xml, 'addChild'));
-        print $xml->asXML();
-    }
-        return  $xml->asXML();
-    }
 }
